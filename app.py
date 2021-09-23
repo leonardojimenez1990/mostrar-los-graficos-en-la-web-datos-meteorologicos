5d25addd8d039f84f39c35387fee744d5226c18d
leonardojimenez1990 <73168995+leonardojimenez1990@users.noreply.github.com>
2021-09-23 14:57:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,62 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
 
-#the web title
+# import plotly.express as px
+
+# the web title
 st.title("Изменение метеорологических величин в течение дня")
 
-#read data wich pandas librery
+# read data wich pandas librery
 file = pd.ExcelFile('leonardoejercicio1.xlsx')
-df=file.parse('Hoja1',header=2, names=['variables','21:00', '00:00', '03:00', '06:00', '09:00', '12:00','15:00','18:00'])
+df = file.parse('Hoja1', header=2,
+                names=['variables', '21:00', '00:00', '03:00', '06:00', '09:00', '12:00', '15:00', '18:00'])
 
 nombreVariables = df['variables']
 st.dataframe(df[0:].astype('str'), width=5000, height=5000)
 
-#realizar la transversal de los datos    
-df=file.parse('Hoja1',header=2, names=['variables','21:00', '00:00', '03:00', '06:00', '09:00', '12:00','15:00','18:00']).T
-#st.table(df.loc[:,['5','6','7']])
+# realizar la transversal de los datos    
+df = file.parse('Hoja1', header=2,
+                names=['variables', '21:00', '00:00', '03:00', '06:00', '09:00', '12:00', '15:00', '18:00']).T
+st.dataframe(df[0:].astype('str'), width=5000, height=5000)
 
-#salve data on variables
+# salve data on variables
 horas = df.index[1:]
-tempSuelo = pd.to_numeric(df.iloc[1:,5]).astype('Float64')
-tempAire = pd.to_numeric(df.iloc[1:,6]).astype('Float64')
-humedad = pd.to_numeric(df.iloc[1:,7]).astype('Float64')
+tempSuelo = pd.to_numeric(df.iloc[:, 5], errors='coerce')
+tempSuelo = pd.DataFrame(tempSuelo)
+tempSuelo.columns = ['Температура почвы, °С']
+# tempSuelo = pd.to_numeric(tempSuelo, errors='coerce')
+tempAire = pd.to_numeric(df.iloc[1:, 6], errors='coerce').astype('Float64')
+humedad = pd.to_numeric(df.iloc[1:, 7], errors='coerce').astype('Float64')
 
-#selectBox
-variable = st.selectbox('Seleccione la variable',[' ','temperatura del suelo','temperatura del aire'])
-if variable == 'temperatura del suelo':
-    #writed from web
-    
+# selectBox
+variable = st.selectbox('выберите переменную', [' ', 'Температура почвы, °С', 'Температура воздуха, °С'])
+if variable == 'Температура почвы, °С':
+    # writed from web
+
     col1, col2 = st.columns(2)
     with col1:
         st.success(variable)
-        st.header("Tabla de datos")
-        st.dataframe(tempSuelo.astype('str'))
-        fig = px.line(tempSuelo[0:], width=500, height=500)
-        fig.show()
-        st.pyplot(fig)
+        st.header("Таблица данных")
+        st.dataframe(tempSuelo.iloc[1:, :])
+        # fig = px.line(tempSuelo[0:], width=500, height=500)
+        # fig.show()
+        # st.pyplot(fig)
     with col2:
-        st.header("Gráfico")
-        
-elif variable == 'temperatura del aire':
-    #writed from web
+        st.header("Графика Температура почвы, °С")
+        st.line_chart(tempSuelo[1:], width=800)
+
+elif variable == 'Температура воздуха, °С':
+    # writed from web
     col1, col2 = st.columns(2)
     with col1:
         st.success(variable)
-        st.header("Tabla de datos")
+        st.header("Таблица данных")
         st.dataframe(tempAire.astype('str'))
     with col2:
-        st.header("Gráfico")
-        st.image("https://static.streamlit.io/examples/cat.jpg")
+        st.header("Графика Температура воздуха, °С")
+        chart_data = pd.DataFrame(
+            np.random.randn(20, 3),
+            columns=['a', 'b', 'c'])
+
+        st.line_chart(chart_data)
